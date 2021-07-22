0834b00e00f51df53465b2116db650c661126ba1
Sasho Spaiyski <sande.sp@abv.bg>
2021-07-22 21:27:35 +0300

    Little changes

---
+++ Java OOP/04.Polymorphism/src/WildFarm/Tiger.java
@@ -0,0 +1,21 @@
+package WildFarm;
+
+public class Tiger extends Felime {
+    public Tiger(String name, String type, double weight, String livingRegion) {
+        super(name, type, weight, livingRegion);
+    }
+
+    @Override
+    public void eat(Food food) {
+        if (food instanceof Vegetable) {
+            throw new IllegalArgumentException(
+                    "Tigers are not eating that type of food!");
+        }
+        super.eat(food);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("ROAAR!!!");
+    }
+}
