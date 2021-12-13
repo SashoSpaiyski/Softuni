38e760d8b705567319e7b38b30769c53348aeee9
Sasho Spaiyski <sande.sp@abv.bg>
2021-12-13 20:35:53 +0200

    Added exercises

---
+++ Algorithms with Java/02.Recursion-and-Combinatorical-algorithms-exercise/src/CombinationsWithoutRepetition.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CombinationsWithoutRepetition {
+    public static int[] arr;
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(reader.readLine());
+        int k = Integer.parseInt(reader.readLine());
+
+        arr = new int[k];
+
+        combinations(0, 1);
+    }
+
+    private static void combinations(int index, int start) {
+        if(index == arr.length){
+            printArr();
+        }else{
+            for (int i = start; i <= n; i++) {
+                arr[index] = i;
+                combinations(index+1, i+1);
+            }
+        }
+    }
+
+    private static void printArr() {
+        for (int el : arr) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+    }
+}
