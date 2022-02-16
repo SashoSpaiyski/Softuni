8c7fa208289e3d94ca35effc5534c146e523dbfd
Sasho Spaiyski <sande.sp@abv.bg>
2022-02-16 15:03:23 +0200

    Added exercises

---
+++ Algorithms Advanced with Java/01.Graphs-Topological-Sorting-MST/src/Kruskal.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Kruskal {
+    public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
+        Collections.sort(edges);
+
+        List<Edge> forest = new ArrayList<>();
+
+        int[] parents = new int[numberOfVertices];
+
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+
+        while (!edges.isEmpty()) {
+            Edge edge = edges.remove(0);
+
+            int source = edge.getStartNode();
+            int dest = edge.getEndNode();
+
+            int firstRoot = findRoot(source, parents);
+            int secondRoot = findRoot(dest, parents);
+
+            if (firstRoot != secondRoot) {
+                forest.add(edge);
+                parents[firstRoot] = secondRoot;
+            }
+
+        }
+
+        return forest;
+    }
+
+    public static int findRoot(int node, int[] parents) {
+
+        while (parents[node] != node) {
+            node = parents[node];
+        }
+
+        return node;
+    }
+}
